b26189ea0c5d729023dc3f3cf4554493e7390fd0
Expertz <xeniapliner@gmail.com>
2023-08-02 23:06:12 +0300

    2 Tasks

---
+++ Cert_1Task/Program.cs
@@ -0,0 +1,108 @@
+﻿/*Задача 1.
+Дан одномерный массив целых чисел, элементом которого, может быть либо 1, либо 0.
+Ваша задача вывести на экран максимальное количество следующих друг за другом 1.
+
+Пример 1:
+Исходный массив: [1, 1, 0, 1, 1, 1]
+Результат: 3
+Объяснение: в данном массиве две последовательности из 1: первые две единицы
+в самом начале и последние три единица в конце. Максимальное количество единиц в этих последовательностях 3.
+
+Пример 2:
+Исходный массив: [1, 0, 1, 1, 0, 1]
+Результат: 2
+
+Пример 3:
+Исходный массив: [1, 0, 1, 0, 0, 0, 1]
+Результат: 0
+Объяснение: в массиве нет следующих друг за другом единиц.*/
+
+
+
+
+/* int GetSequenceOfOnes(int[] array)
+    {
+        int count = 0;
+        int maxCount = 0;
+
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == 1)
+        {
+            count++;
+            if (count > maxCount)
+            {
+                maxCount = count;
+            }
+        }
+        else
+        {
+            if (count > 0)
+            {
+                count = 0;
+            }
+        }
+    }
+    return maxCount;
+}
+
+ int[] array = { 1, 0, 1, 0, 0, 0, 1 };
+ int maxCount = GetSequenceOfOnes(array);
+ Console.WriteLine(maxCount);  */ 
+
+ /*int[] array = {1, 0, 1, 0, 0, 0, 1};
+        int maxCount = 0;
+        int currentCount = 0;
+        
+        foreach (int num in array)
+        {
+            if (num == 1)
+            {
+                currentCount++;
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                }
+            }
+            else
+            {
+                currentCount = 0;
+            }
+        }
+        
+        Console.WriteLine("Quantity of sequences ones is: " + maxCount);*/
+
+/*int[] array = { 1, 1, 0, 1, 1, 1 };
+        
+        int maxCount = 0;
+        int currentCount = 0;
+        bool isInSequence = false;
+        
+        foreach (int num in array)
+        {
+            if (num == 1)
+            {
+                if (isInSequence)
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    currentCount = 1;
+                    isInSequence = true;
+                }
+                
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                }
+            }
+            else
+            {
+                isInSequence = false;
+            }
+        }
+        
+        Console.WriteLine("The quantity of sequenced ones in array is: " + maxCount);*/
+        
+
---
+++ Cert_2Task/Program.cs
@@ -0,0 +1,59 @@
+﻿/*Дан одномерный массив целых однозначных чисел (элемент массива содержит число от 1 до 9).
+Ваша задача вывести на экран число, которое формируется элементами массива слева-направо + 1.
+
+Пример 1:
+Исходный массив: [1, 2, 1, 1, 4]
+Результат: 12115 (целое число).
+Объяснение: если мы пройдем по элементам массива слева-направо, то последовательность элементов представляет из себя число 12004. Дальше мы плюсуем к этому числу 1 и получаем 12115
+
+Пример 2:
+Исходный массив: [1, 1, 2, 9]
+Результат: 1130
+Объяснение: если мы пройдем по массиву слева-направо, то получим число 1129. Прибавляем к данному числу 1 и получаем 1130.
+
+Пример 3:
+Исходный массив: [9]
+Результат: 10. */
+
+// Methods
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int[] GetDoubleArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+void PrintIntArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+    Console.WriteLine();
+}
+double GetNumber(int[] array)
+{
+    double sum = 0;
+    int NumPow = 0;
+    for (int i = array.Length - 1; i > -1; i--)
+    {
+        sum = sum + array[i] * Math.Pow(10, NumPow);
+        NumPow++;
+    }
+    return sum;
+}
+//------------------------------------------------------------
+int length = Prompt("Put your array length");
+int[] array = GetDoubleArray(length);
+PrintIntArray(array);
+double sum = GetNumber(array);
+Console.WriteLine($"The number you are seeking is {sum + 1}");
